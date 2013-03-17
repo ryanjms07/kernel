6569f2296fdcba1b1cd2f84b27ae65295410381f
ryanjms07 <ryan_jms07@yahoo.com>
2013-03-17 18:00:36 +0700

    sorry i don'y changes this folder

---
+++ common/init/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_init/built-in.o :=  /home/ryanjms07/sgy/arm-2009q3/bin/arm-none-eabi-ld -EL    -r -o init/built-in.o init/main.o init/version.o init/mounts.o init/initramfs.o init/calibrate.o 
---
+++ common/init/.mounts.o.cmd
@@ -0,0 +1,1 @@
+cmd_init/mounts.o := /home/ryanjms07/sgy/arm-2009q3/bin/arm-none-eabi-ld -EL    -r -o init/mounts.o init/do_mounts.o init/do_mounts_rd.o init/do_mounts_initrd.o 
