0a00533ad0f5ae2ea15710de405c7c6f284f840b
ryanjms07 <ryan_jms07@yahoo.com>
2013-03-18 07:56:45 +0700

    not any changes

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 35 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#35 PREEMPT Thu Feb 21 04:05:17 PST 2013"
+#define LINUX_COMPILE_TIME "04:05:17"
+#define LINUX_COMPILE_BY "ryanjms07"
+#define LINUX_COMPILE_HOST "Hm&KernelV5"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
